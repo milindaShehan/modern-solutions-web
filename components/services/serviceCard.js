859f8bfc5d9117d18dc13a4372a69e2a58cfccf7
milindaShehan <milinda.msd@gmail.com>
2024-11-23 01:34:50 +0530

    service fix

--- components/services/serviceCard.js
+++ components/services/serviceCard.js
@@ -3,9 +3,9 @@ import React from "react";
 const ServiceCard = ({ title, description, icon }) => {
   return (
 
-      <div className="rounded-lg bg-white  shadow-sm p-5 flex flex-col  items-center w-xs h-60 transition-transform duration-300 hover:scale-105">
+      <div className="rounded-2xl bg-white  shadow-sm p-5 flex flex-col  items-center w-xs h-60 transition-transform duration-300 hover:scale-105 cursor-pointer">
         <div className="flex justify-center items-center mb-4 w-6 h-6 rounded-full bg-primary lg:h-12 lg:w-12 ">
-          <div className="text-Primary">{icon}</div>
+          <div className="text-white">{icon}</div>
         </div>
         <h3 className="mb-2 text-lg font-bold text-gray-700  text-center">
           {title}
