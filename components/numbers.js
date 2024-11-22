b9a111086f96bd679fdb32594af7ddb570fdfe1b
milindaShehan <milinda.msd@gmail.com>
2024-11-22 19:02:53 +0530

    remove countup

--- components/numbers.js
+++ components/numbers.js
@@ -1,31 +1,23 @@
 "use client";
 import React from 'react';
-import CountUp from 'react-countup';
 
 const Numbers = () => {
     return (
         <section id='numbers'>
-            <div className=" px-4 py-8 text-center ">
-                <dl className="grid gap-8  w-full text-gray-900 sm:grid-cols-3 py-4 px-20 rounded-xl bg-gray-200">
+            <div className="max-w-screen-xl px-4 pb-8 mx-auto text-center lg:pb-16 lg:px-6">
+                <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 py-8 px-20 rounded-xl bg-gray-50">
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-                            <CountUp end={50} duration={2.5} suffix="+" />
-                        </dt>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">5000+</dt>
                         <dd className="font-light text-gray-500">happy customers</dd>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-                            <CountUp end={5} duration={2.5} suffix="+" />
-                        </dt>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">5+</dt>
                         <dd className="font-light text-gray-500">years in service</dd>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-                            <CountUp end={50} duration={2.5} suffix="+" />
-                        </dt>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">50+</dt>
                         <dd className="font-light text-gray-500">tuk-tuks & scooters</dd>
                     </div>
-                    
                 </dl>
             </div>
         </section>
