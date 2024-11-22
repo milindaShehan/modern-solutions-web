beeeb4ee543215d1eaf986a74e174be721dd4538
milindaShehan <milinda.msd@gmail.com>
2024-11-22 17:19:07 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#ffffff", // Light background color
+        foreground: "#000000", // Primary text color
+        primary: "#FE6D31",
+         // Added primary color
+        secondary: "#fee0d4", // Example secondary color (gray)
+        accent: "#fbbf24", // Example accent color (yellow)
+        muted: "#f3f4f6", // Muted background color for sections
+        border: "#e5e7eb", // Light border color
+        text: {
+          light: "#6b7280", // Lighter text color for secondary text
+          dark: "#374151", // Darker text color for primary text
+        },
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // Adding Poppins to the sans font family
+      },
+    },
+  },
+  plugins: [],
+};
