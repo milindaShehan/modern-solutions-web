3e53d5900aba41bd6f50f467436ec3d94d3ce3d2
milindaShehan <milinda.msd@gmail.com>
2024-11-22 19:45:27 +0530

    add content

--- components/numbers.js
+++ components/numbers.js
@@ -4,19 +4,19 @@ import React from 'react';
 const Numbers = () => {
     return (
         <section id='numbers'>
-            <div className="max-w-screen-xl px-4 pb-8 mx-auto text-center lg:pb-16 lg:px-6">
-                <dl className="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 py-8 px-20 rounded-xl bg-gray-50">
+            <div className=" px-4 pb-8 mx-auto text-center lg:pb-16 lg:px-6">
+                <dl className="grid gap-12 mx-auto text-gray-900 sm:grid-cols-3 py-6 px-20 rounded-xl bg-gray-50">
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">5000+</dt>
-                        <dd className="font-light text-gray-500">happy customers</dd>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">100+</dt>
+                        <dd className="font-light text-gray-500">Businesses Digitalized</dd>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">5+</dt>
-                        <dd className="font-light text-gray-500">years in service</dd>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">300+</dt>
+                        <dd className="font-light text-gray-500">Successful Events Managed</dd>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">50+</dt>
-                        <dd className="font-light text-gray-500">tuk-tuks & scooters</dd>
+                        <dt className="mb-2 text-3xl md:text-4xl font-extrabold">500+</dt>
+                        <dd className="font-light text-gray-500">Customer Interactions via Hotline</dd>
                     </div>
                 </dl>
             </div>
