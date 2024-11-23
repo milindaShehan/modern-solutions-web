16c9bba878ce17181a91a22f51f401ed60f5e7d5
milindaShehan <milinda.msd@gmail.com>
2024-11-23 16:12:30 +0530

    numbers

--- components/numbers.js
+++ components/numbers.js
@@ -58,24 +58,30 @@ const Numbers = () => {
   return (
     <section id="numbers">
       <div className="px-4 pb-8 mx-auto text-center lg:pb-16 lg:px-6">
-        <dl className="grid gap-12 mx-auto text-gray-900 sm:grid-cols-3 py-6 px-20 rounded-xl bg-white border border-t border-b">
+        <dl className="grid gap-12 mx-auto text-gray-900 sm:grid-cols-4 py-6 px-20 rounded-xl bg-white border border-t border-b">
           <div className="flex flex-col items-center justify-center">
             <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-              <Counter end={100} duration={2000} />+
+              <Counter end={20} duration={2000} />+
             </dt>
             <dd className="font-light">Businesses Digitalized</dd>
           </div>
           <div className="flex flex-col items-center justify-center">
             <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-              <Counter end={300} duration={2000} />+
+              <Counter end={25} duration={2000} />+
             </dt>
             <dd className="font-light">Successful Events Managed</dd>
           </div>
           <div className="flex flex-col items-center justify-center">
             <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
-              <Counter end={500} duration={2000} />+
+              <Counter end={12} duration={2000} />+
             </dt>
             <dd className="font-light">Customer Interactions via Hotline</dd>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <dt className="mb-2 text-3xl md:text-4xl font-extrabold">
+              <Counter end={80} duration={2000} />+
+            </dt>
+            <dd className="font-light">Employee Recruitments</dd>
           </div>
         </dl>
       </div>
